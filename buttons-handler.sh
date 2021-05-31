7707db8265106edb02b3c44f367c00dc04f4beb3
avhaliullin <avhaliullin@users.noreply.github.com>
2021-05-31 10:26:31 +0300

    Create buttons-handler.sh

---
+++ buttons-handler.sh
@@ -0,0 +1,20 @@
+echo '{
+  "version": "1.0",
+  "response": {
+    "text": "Посмотри на мои кнопки",
+    "buttons": [
+      {
+        "title": "Кнопка 1",
+        "payload": {
+          "a": "b"
+        }
+      },
+      {
+        "title": "Кнопка 2",
+        "payload": {
+          "a": "c"
+        }
+      }
+    ]
+  }
+}'
